e0401818a85bdded5927a992a8635d1c45296406
Zaur Malakhov <1zaur1@mail.ru>
2020-12-21 07:31:22 +0300

    Сделал модальное окно

--- source/js/main.js
+++ source/js/main.js
@@ -12,3 +12,26 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+const addToCartLinks = document.querySelectorAll('.add-to-cart');
+const addToCartPopup = document.querySelector('.modal__form-cart');
+const addToCartClose = document.querySelector('.form-cart__button');
+
+// console.log(addToCartLink);
+
+for (let i = 0; i < addToCartLinks.length; i++) {
+  addToCartLinks[i].addEventListener("click",(evt) => {
+    evt.preventDefault();
+    addToCartPopup.classList.add("modal-show");
+  });
+
+}
+
+addToCartClose.addEventListener("click",(evt) => {
+  evt.preventDefault();
+
+  // добавляем в корзину
+
+  //закрываем окно
+  addToCartPopup.classList.remove("modal-show");
+});
